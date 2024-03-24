345862d77be5b4b8324486322fc373ac98c4f65f
Amirhossein <4mirh03einhashemi@gmail.com>
2024-03-24 15:47:19 +0330

    🔨 create AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    // it is layout route.
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello world</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
