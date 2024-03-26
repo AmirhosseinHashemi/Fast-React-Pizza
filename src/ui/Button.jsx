60fa94299e3bb2011ea2437d8b9c4da9bf87e7a3
Amirhossein <4mirh03einhashemi@gmail.com>
2024-03-26 23:07:44 +0330

    💄 style cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disable, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 ring-yellow-300 ring-offset-2 transition hover:bg-yellow-300 focus:outline-none focus:ring disabled:cursor-not-allowed";
+    "text-center inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 ring-yellow-300 ring-offset-2 transition hover:bg-yellow-300 focus:outline-none focus:ring disabled:cursor-not-allowed focus:bg-yellow-300";
 
   const styles = {
     primary: base + " px-4 py-3 sm:px-6 sm:py-4",
     small: base + " px-3 py-2 sm:px-5 sm:py-3 text-xs",
+    secondary:
+      "px-4 sm:px-6 inline-block rounded-full font-semibold uppercase tracking-wider text-stone-300 ring-stone-300 ring-offset-2 transition hover:bg-stone-300 focus:outline-none focus:ring disabled:cursor-not-allowed hover:text-stone-800 border-2 border-stone-300 focus:bg-stone-300 focus:text-stone-800",
   };
 
   if (to)
